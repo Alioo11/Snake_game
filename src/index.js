f896dfa7bde6dd4b24fce5bc0b6bb35160fbaf7b
Ali Salehi <110mors110@gmail.com>
2022-02-01 21:59:26 +0330

    initialized game board

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import {hello} from './helpers/module'
+//import {hello} from './helpers/module'
 import "./main.css"
-hello()
 
-//alert('this is the index js file ')
+
+const board = document.querySelector('.board')
+
+const createBoard = (width, height) => {
+    board.innerHTML = Array.from(Array.from(Array(width * height).keys()), (item) => `<div id='${item}' class='cell'></div>`).join().replace(/,/g, "")
+};
+
+createBoard(39,30)
+
+console.log(board)
